dc8fe944985830a27aff228375b1a53edddc3872
jlim10343 <aiwen.joy.lim@gmail.com>
2019-03-25 14:26:09 -0400

    Finished the javadoc comments for Sessions class

--- app/src/main/java/com/example/voluntutor/Sessions.java
+++ app/src/main/java/com/example/voluntutor/Sessions.java
@@ -2,6 +2,9 @@ package com.example.voluntutor;
 
 import java.util.Date;
 
+/**
+ * Stores information about a tutoring session (date, location, length, verified status, tutor, and tutee)
+ */
 public class Sessions {
     //data
     private Date date;
@@ -21,16 +24,75 @@ public class Sessions {
         this.tutee = tutee;
     }
     //methods
+
+    /**
+     * Gets the date of the session
+     * @return the date of the session
+     */
     public Date getDate() {return date;}
+
+    /**
+     * Gets the location of the session
+     * @return the location of the session
+     */
     public String getLoc() {return location;}
+
+    /**
+     * Gets the length of time the session is (in minutes)
+     * @return the length of the session in minutes
+     */
     public int getLength() {return length;}
+
+    /**
+     * Gets whether the session is verified
+     * @return whether the session is verified
+     */
     public boolean getVerified() {return verified;}
+
+    /**
+     * Gets the name of the tutor
+     * @return the name of the tutor
+     */
     public String getTutor() {return tutor;}
+
+    /**
+     * Gets the name of the tutee
+     * @return the name of the tutee
+     */
     public String getTutee() {return tutee;}
+
+    /**
+     * Sets verified status to true
+     */
     public void verify() {verified = true;}
+
+    /**
+     * Sets the date of the session
+     * @param d the date of the session
+     */
     public void setDate(Date d) {date = d;}
+
+    /**
+     * Sets the location of the session
+     * @param loc the location of the session
+     */
     public void setLocation(String loc) {location = loc;}
+
+    /**
+     * Sets the length of the session in minutes
+     * @param len the length of the session in minutes
+     */
     public void setLength(int len) {length = len;}
+
+    /**
+     * Sets the name of the tutor
+     * @param t the name of the tutor
+     */
     public void setTutor(String t) {tutor = t;}
+
+    /**
+     * Sets the name of the tutee
+     * @param s the name of the tutee
+     */
     public void setTutee(String s) {tutee = s;}
 }
