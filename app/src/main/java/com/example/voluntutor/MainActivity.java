7b9c6c87cc4896b60e6b727753bd48172a94f16e
sjgeo <svjonnalagadda@wpi.edu>
2019-04-11 21:39:47 -0400

    FireBase Works(retrieving data as well)

--- app/src/main/java/com/example/voluntutor/MainActivity.java
+++ app/src/main/java/com/example/voluntutor/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.voluntutor;
 
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
@@ -8,14 +7,26 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+//import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 
 /**
  * This class allows for the UI to run in compliance with the individual fragments and the bottom navigation bar
  */
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
     /**
      * This method allows the GUI to load individual fragments and switch between views
@@ -42,10 +53,14 @@ public class MainActivity extends AppCompatActivity {
      * This method sets the content views, opens the settings button, sets the content views
      * @param savedInstanceState
      */
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         loadFragment(new HomeFragment());
 
@@ -61,12 +76,38 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
+
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+        myRef.setValue("Hello?");
+
+        // Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                Log.d("SaquethTag","the message is: " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+
+
     }
+
 
     /**
      * This method opens the settings preference screen
      */
-    public void openSettings ()
+   public void openSettings ()
     {
         Intent intent = new Intent(this, SettingsPreference.class);
         startActivity(intent);
@@ -102,6 +143,7 @@ public class MainActivity extends AppCompatActivity {
                     return true;
                 }
             };
+
 
     /**
      * This creates a new instance of the preference fragment class for the settings preference fragment
