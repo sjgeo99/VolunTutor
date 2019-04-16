804b1e3454f5b9c22ee13c80ea95e865506b7670
jlim10343 <aiwen.joy.lim@gmail.com>
2019-04-16 14:58:52 -0400

    Added a new settings page with buttons that we can add methods to in order to update user settings (ie name, school, etc). They are not yet connected, though

--- app/src/main/java/com/example/voluntutor/MainActivity.java
+++ app/src/main/java/com/example/voluntutor/MainActivity.java
@@ -1,13 +1,17 @@
 package com.example.voluntutor;
 
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -60,7 +64,6 @@ public class MainActivity extends AppCompatActivity {
                 openSettings();
             }
         });
-
     }
 
     /**
@@ -74,7 +77,7 @@ public class MainActivity extends AppCompatActivity {
 
 
     /**
-     * This method allows the bottom navigation bar to be acessed on each screen, and
+     * This method allows the bottom navigation bar to be accessed on each screen, and
      * allows for the chosen screens to be displayed
      */
     private BottomNavigationView.OnNavigationItemSelectedListener navListener =
@@ -94,6 +97,9 @@ public class MainActivity extends AppCompatActivity {
                             selectedFragment = new HoursFragment();
                             break;
 
+                        case R.id.nav_settings:
+                            selectedFragment = new SettingsFragment();
+                            break;
                     }
 
                     getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
